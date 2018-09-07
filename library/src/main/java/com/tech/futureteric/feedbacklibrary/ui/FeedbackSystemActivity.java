07c21980e4449af33e5d6ed3995ec3a9fee42213
Dasser Basyouni <daser4ever96@gmail.com>
2018-09-07 18:55:54 +0200

    refactor: Removed viewBinders name form package
    - Removed viewBinders name form package.
    - Moved FeedbackSystemActivity to ui package.

--- library/src/main/java/com/tech/futureteric/feedbacklibrary/ui/FeedbackSystemActivity.java
+++ library/src/main/java/com/tech/futureteric/feedbacklibrary/ui/FeedbackSystemActivity.java
@@ -1,4 +1,4 @@
-package com.tech.futureteric.feedbacklibrary.ui.butterknife;
+package com.tech.futureteric.feedbacklibrary.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
